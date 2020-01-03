d748c63ace8145b4d183427ee35f4c62eed693f1
Daniel Nascimento <danascimen@thoughtworks.com>
2020-01-03 13:45:12 -0300

    [0019] Adding user class and tests

---
+++ project/main/User.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class User {
+  function users_len(){
+    return 28
+  }
+}
---
+++ project/tests/UserTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class UserTest{
+  test("User.users_len") {
+    assert(User.cuusers_lenbe(3) === 27)
+  }
+}
